e9c66eae70e343a791204495d53022f87d1ff4f9
이용목 <yongmok@iyongmog-ui-MacBook-Pro.local>
2017-07-03 13:45:07 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int inp = input.nextInt();
+
+        System.out.println(inp);
+
+        for(int i  = 0 ; i<inp ; i++){
+            System.out.println("Hello World!");
+        }
+    }
+}
